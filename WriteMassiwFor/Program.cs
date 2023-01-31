58229f6f976aee61e14a73cc67b7f13a9ba83f3a
iriguss <95167159+iriguss@users.noreply.github.com>
2023-01-31 23:26:03 +0500

    добавила примеры для использования разных операций

---
+++ WriteMassiwFor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WriteMassiwFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] names = new string[13];
+
+            //names[0] = "Ivan";
+
+            //Console.WriteLine("Введите ваше имя:");
+            //names[1] = Console.ReadLine();
+
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            int[] numbers = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next();
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
